28b54a86d785e235fb6f0eb5f3868fd9712050c9
Mohamed Elgendy <mooelgendy@gmail.com>
2020-05-31 23:00:49 +0200

    Modify controller

--- src/main/java/com/myfaculty/myapp/HomeController.java
+++ src/main/java/com/myfaculty/myapp/HomeController.java
@@ -17,188 +17,209 @@ public class HomeController {
 	// Home
 	@GetMapping("/")
 	public ModelAndView home() {
+		ModelAndView modelAndView = null;
 		try {
-			return new ModelAndView("home", MODEL_NAME, new Object());
+			modelAndView = new ModelAndView("home", MODEL_NAME, new Object());
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	// Departments Requests
 	@GetMapping("/addDepartment")
 	public ModelAndView addDepartment(Department d) {
+		ModelAndView modelAndView = null;
 		try {
-			return new ModelAndView("addDepartment", MODEL_NAME, d);
+			modelAndView = new ModelAndView("addDepartment", MODEL_NAME, d);
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	@PostMapping("/save1")
 	public ModelAndView saveDepartment(@ModelAttribute("d") Department d) {
+		ModelAndView modelAndView = null;
 		try {
 			di.insertDepartment(d);
-			return new ModelAndView("redirect:/viewDepartment");
+			modelAndView = new ModelAndView("redirect:/viewDepartment");
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	@GetMapping("/viewDepartment")
 	public ModelAndView viewDepartment() {
+		ModelAndView modelAndView = null;
+		List<Department> departmentList = null;
 		try {
-			List<Department> list1 = di.viewAllDepartments();
-			return new ModelAndView("viewDepartment", "list1", list1);
+			departmentList = di.viewAllDepartments();
+			modelAndView = new ModelAndView("viewDepartment", "list1", departmentList);
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	@GetMapping("/deleteDepartment/{depId}")
 	public ModelAndView deleteDepartment(@PathVariable int depId) {
+		ModelAndView modelAndView = null;
 		try {
 			di.deleteDepartment(depId);
-			return new ModelAndView("redirect:/viewDepartment");
+			modelAndView = new ModelAndView("redirect:/viewDepartment");
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	// Courses Requests
 	@GetMapping("/addCourses")
 	public ModelAndView addCourses(Courses c) {
+		ModelAndView modelAndView = null;
 		try {
-			return new ModelAndView("addCourses", MODEL_NAME, c);
+			modelAndView = new ModelAndView("addCourses", MODEL_NAME, c);
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	@PostMapping(value = "/save2")
 	public ModelAndView saveCourses(@ModelAttribute("c") Courses c) {
+		ModelAndView modelAndView = null;
 		try {
 			di.insertCourses(c);
-			return new ModelAndView("redirect:/viewCourses");
+			modelAndView = new ModelAndView("redirect:/viewCourses");
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	@GetMapping("/viewCourses")
 	public ModelAndView viewCourses() {
+		ModelAndView modelAndView = null;
+		List<Courses> coursesList = null;
 		try {
-			List<Courses> list2 = di.viewAllCourses();
-			return new ModelAndView("viewCourses", "list2", list2);
+			coursesList = di.viewAllCourses();
+			modelAndView = new ModelAndView("viewCourses", "list2", coursesList);
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	@GetMapping("/deleteCourses/{coId}")
 	public ModelAndView deleteCourses(@PathVariable int coId) {
+		ModelAndView modelAndView = null;
 		try {
 			di.deleteCourses(coId);
-			return new ModelAndView("redirect:/viewCourses");
+			modelAndView = new ModelAndView("redirect:/viewCourses");
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	// Instructors Requests
 	@GetMapping("/addInstructors")
 	public ModelAndView addInstructors(Instructors i) {
+		ModelAndView modelAndView = null;
 		try {
-			return new ModelAndView("addInstructors", MODEL_NAME, i);
+			modelAndView = new ModelAndView("addInstructors", MODEL_NAME, i);
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	@PostMapping("/save3")
 	public ModelAndView saveInstructors(@ModelAttribute("i") Instructors i) {
+		ModelAndView modelAndView = null;
 		try {
 			di.insertInstructor(i);
-			return new ModelAndView("redirect:/viewInstructors");
+			modelAndView = new ModelAndView("redirect:/viewInstructors");
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	@GetMapping("/viewInstructors")
 	public ModelAndView viewInstructors() {
+		ModelAndView modelAndView = null;
+		List<Instructors> instructorsList = null;
 		try {
-			List<Instructors> list3 = di.viewAllInstructors();
-			return new ModelAndView("viewInstructors", "list3", list3);
+			instructorsList = di.viewAllInstructors();
+			modelAndView = new ModelAndView("viewInstructors", "list3", instructorsList);
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	@GetMapping("deleteInstructor/{insId}")
 	public ModelAndView deleteInstructors(@PathVariable int insId) {
+		ModelAndView modelAndView = null;
 		try {
 			di.deleteInstructor(insId);
-			return new ModelAndView("redirect:/viewInstructors");
+			modelAndView = new ModelAndView("redirect:/viewInstructors");
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	// Students Requests
 	@GetMapping("/addStudents")
 	public ModelAndView addStudents(Students s) {
+		ModelAndView modelAndView = null;
 		try {
-			return new ModelAndView("addStudents", MODEL_NAME, s);
+			modelAndView = new ModelAndView("addStudents", MODEL_NAME, s);
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	@PostMapping("/save4")
 	public ModelAndView saveStudents(@ModelAttribute("s") Students s) {
+		ModelAndView modelAndView = null;
 		try {
 			di.insertStudent(s);
-			return new ModelAndView("redirect:/viewStudents");
+			modelAndView = new ModelAndView("redirect:/viewStudents");
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	@GetMapping("/viewStudents")
 	public ModelAndView viewStudents() {
+		ModelAndView modelAndView = null;
+		List<Students> studentsList = null;
 		try {
-			List<Students> list4 = di.viewAllStudents();
-			return new ModelAndView("viewStudents", "list4", list4);
+			studentsList = di.viewAllStudents();
+			modelAndView = new ModelAndView("viewStudents", "list4", studentsList);
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 
 	@GetMapping("/deleteStudents/{studId}")
 	public ModelAndView deleteStudents(@PathVariable int studId) {
+		ModelAndView modelAndView = null;
 		try {
 			di.deleteStudent(studId);
-			return new ModelAndView("redirect:/viewStudents");
+			modelAndView = new ModelAndView("redirect:/viewStudents");
 		} catch (Exception e) {
-			System.out.println(e.toString());
+			System.out.println(e.getStackTrace());
 		}
-		return new ModelAndView();
+		return modelAndView;
 	}
 }
 	//Log in
